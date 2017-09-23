1d1515a0ccab61b0d21c217dea4335266a398741
Zachery Williams <zachery.williams@beltonnewtech.org>
2017-09-23 11:01:31 -0500

    added extra code probably useless

--- RobotSource.c
+++ RobotSource.c
@@ -1,3 +1,13 @@
+≈#pragma config(Sensor, dgtl1,  ,               sensorTouch)
+#pragma config(Motor,  port2,            ,             tmotorServoContinuousRotation, openLoop)
+#pragma config(Motor,  port3,            leftwheels,   tmotorServoContinuousRotation, openLoop)
+#pragma config(Motor,  port4,            ,             tmotorServoContinuousRotation, openLoop)
+#pragma config(Motor,  port5,            ,             tmotorServoContinuousRotation, openLoop)
+#pragma config(Motor,  port6,            ,             tmotorServoContinuousRotation, openLoop)
+#pragma config(Motor,  port7,            ,             tmotorServoContinuousRotation, openLoop)
+#pragma config(Motor,  port8,            ,             tmotorServoContinuousRotation, openLoop)
+#pragma config(Motor,  port9,            rightwheels,  tmotorServoContinuousRotation, openLoop)
+
 task main()
 {
 
@@ -5,8 +15,8 @@ task main()
 	{
 		//Port 3 is left and Port 9 is Right
 
-		motor[port3] = vexRT[Ch3];
-		motor[port9] = vexRT[Ch2];
+		motor[port3] = vexRT[Ch3]; 
+		motor[port9] = vexRT[Ch2]; 
 
 	}
 }
