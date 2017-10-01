b658f9d6fac9bfc01e09fe60a1f6cac13b3c2123
Anonymouqs <jehbar.ibarra@gmail.com>
2017-10-01 10:55:00 -0500

    Side pop-up changed from px to percent

--- website/js/sidebar.js
+++ website/js/sidebar.js
@@ -1,15 +1,16 @@
-var OPEN_WIDTH = "250px";
-var CLOSE_WIDTH = "0px";
+var OPEN_WIDTH = "25%";
+var CLOSE_WIDTH = "0%";
 
 function openNav()
 {
     document.getElementById("navigation").style.width = OPEN_WIDTH;
-    document.getElementById("antiNav").style.paddingLeft = OPEN_WIDTH;
+    document.getElementById("navigation").style.display = "block";
+    document.getElementById("antiNav").style.marginLeft = OPEN_WIDTH;
 }
 
 function closeNav()
 {
     document.getElementById("navigation").style.width = CLOSE_WIDTH;
-    document.getElementById("antiNav").style.paddingLeft = CLOSE_WIDTH;
+    document.getElementById("antiNav").style.marginLeft = CLOSE_WIDTH;
 
 }
