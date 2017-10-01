03dbff01de90d2da3cb47a75fe3e304f29995227
Anonymouqs <jehbar.ibarra@gmail.com>
2017-09-30 22:05:12 -0500

    Large Update: Added Functional Navigation Bar. Added Site Page Hierarchy. External Javascript Impletemented.

--- website/js/sidebar.js
+++ website/js/sidebar.js
@@ -1,10 +1,15 @@
+var OPEN_WIDTH = "250px";
+var CLOSE_WIDTH = "0px";
 
 function openNav()
 {
-    document.getElementById("navigation").width = "250px"
+    document.getElementById("navigation").style.width = OPEN_WIDTH;
+    document.getElementById("antiNav").style.paddingLeft = OPEN_WIDTH;
 }
+
 function closeNav()
 {
-    document.getElementById("navigation").width = "0px"
-    alter("close")
+    document.getElementById("navigation").style.width = CLOSE_WIDTH;
+    document.getElementById("antiNav").style.paddingLeft = CLOSE_WIDTH;
+
 }
