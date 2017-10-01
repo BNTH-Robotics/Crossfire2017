5588de426a58e1b889a1e5d3017683fbe4412af4
Anonymouqs <jehbar.ibarra@gmail.com>
2017-09-30 21:33:37 -0500

    Navigaiton with Javascript Experimentation

---
+++ website/js/sidebar.js
@@ -0,0 +1,10 @@
+
+function openNav()
+{
+    document.getElementById("navigation").width = "250px"
+}
+function closeNav()
+{
+    document.getElementById("navigation").width = "0px"
+    alter("close")
+}
