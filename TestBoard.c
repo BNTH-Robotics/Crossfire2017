c9fa1a2cffea9b5bc50c2bdb456bcd29f37aa248
prestonbr0wn <preston.brown@beltonnewtech.org>
2017-10-02 16:44:15 -0500

    Set up

---
+++ TestBoard.c
@@ -0,0 +1,12 @@
+#pragma config(Motor, port1, Sm1, tmotorServoContinuousRotation, openLoop)
+#pragma config(Motor, port2, Sm2, tmotorServoContinuousRotation, openLoop)
+#pragma config(Motor, port3, Lm1, tmotorServoContinuousRotation, openLoop)
+#pragma config(Motor, port4, Lm2, tmotorServoContinuousRotation, openLoop)
+#pragma config(Servo, port5, S1, tmotorServoContinuousRotation, openLoop)
+#pragma config(Servo, port6, S2, tmotorServoContinuousRotation, openLoop)
+#pragma config(Servo, port7, S3, tmotorServoContinuousRotation, openLoop)
+#pragma config(Servo, port8, S4, tmotorServoContinuousRotation, openLoop)
+task main()
+{
+
+}
