57b40eb8c6a8a40d53b2a08013c872d13d0571b5
Anonymouqs <jehbar.ibarra@gmail.com>
2017-10-02 12:38:48 -0500

    Navigation experiment: Opens when mouse hovers. Closes when mouse exits

--- website/js/sidebar.js
+++ website/js/sidebar.js
@@ -1,16 +1,21 @@
 var OPEN_WIDTH = "25%";
-var CLOSE_WIDTH = "0%";
+var CLOSE_WIDTH = "1%";
+var navLink = document.getElementsByClassName("navigation a");
+var navlinkposOpen = "0%"
+var navlinkposClose = "700%"
 
 function openNav()
 {
     document.getElementById("navigation").style.width = OPEN_WIDTH;
     document.getElementById("navigation").style.display = "block";
     document.getElementById("antiNav").style.marginLeft = OPEN_WIDTH;
+    navLink.style.right(navlinkposOpen);
 }
 
 function closeNav()
 {
     document.getElementById("navigation").style.width = CLOSE_WIDTH;
     document.getElementById("antiNav").style.marginLeft = CLOSE_WIDTH;
+    navLinkink.style.right(navlinkposClose);
 
 }
