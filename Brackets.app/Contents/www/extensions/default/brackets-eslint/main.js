dec378001707e67cd32be96dd8e6f15b6d2b10b3
Seils Evan <evan.seils@belton.isd>
2017-10-05 16:33:35 -0500

    Remove Overhead

--- Brackets.app/Contents/www/extensions/default/brackets-eslint/main.js
+++
@@ -1,4 +0,0 @@
-define(function (require, exports, module) {
-  require('./node_modules/brackets-inspection-gutters/dist/main')();
-  require('./dist/main');
-});
--- Robot Mesh/pymite-09/src/platform/pic24/include/devices/pic24fj128ga006_pyports.h
+++
@@ -1,5 +0,0 @@
-#define AN_CN_MAP \
-  /*   AN pin   ,    CN pin    */ \
-  /* -----------, ------------ */
-
-#define _PIC24_PYDIGIO_DEFINED
